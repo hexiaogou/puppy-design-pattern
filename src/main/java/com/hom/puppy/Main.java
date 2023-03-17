6b19957320925dfb6953307a6b12659072cab258
何霖 <lin12.he@tcl.com>
2023-03-17 19:04:55 +0800

    add main java

---
+++ src/main/java/com/hom/puppy/Main.java
@@ -0,0 +1,14 @@
+package com.hom.puppy;
+
+/**
+ * Main
+ *
+ * @author hexiaogou
+ * @desc main
+ * @date 2023-03-17 19:03
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world.");
+    }
+}
